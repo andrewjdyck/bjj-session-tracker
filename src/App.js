f6ea463c1c420a1ce7b53be6a52e3df9aae4069c
andrewjdyck <andrew@andrewdyck.com>
2024-01-27 20:38:17 -0600

    Basic app functionality setup with logging sessions.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+// import {Routes, Route} from 'react-router-dom';
 import SessionForm from './components/SessionForm';
 import SessionList from './components/SessionList';
-import SignIn from './components/SignIn';
+// import SignIn from './components/SignIn';
+// import SignUp from './components/SignUp';
 import { auth, firestore } from './firebase';  // Import firestore from firebase.js
 import './App.css';
+
 
 
 function App() {
@@ -11,13 +15,29 @@ function App() {
   const [sessions, setSessions] = useState([]);
 
   // Set up a listener to update the user state when authentication status changes
-  auth.onAuthStateChanged((currentUser) => {
-    setUser(currentUser);
-  });
-
+  // This listener is called when the app starts and every time a user signs in or out
+  // UseEffect for auth state changes
   useEffect(() => {
+    const unsubscribeAuth = auth.onAuthStateChanged((currentUser) => {
+      console.log('onAuthStateChanged called with currentUser:', currentUser);
+      setUser(currentUser);
+      if (currentUser) {
+        loadUserSessions(currentUser);
+      }
+    });
+
+    // Cleanup function
+    return () => unsubscribeAuth();
+  }, []);
+
+  // this sets up a real-time listener to a firestore database
+  // UseEffect for Firestore listener
+  useEffect(() => {
+    let unsubscribeFirestore = () => {};
+
     if (user) {
-      const unsubscribe = firestore
+      console.log("useEffect called with user:", user);
+      unsubscribeFirestore = firestore
         .collection('sessions')
         .doc(user.uid)
         .collection('userSessions')
@@ -26,14 +46,15 @@ function App() {
           const userSessions = snapshot.docs.map((doc) => doc.data());
           setSessions(userSessions);
         });
-
-      return () => unsubscribe();
     }
+
+    // Cleanup function
+    return () => unsubscribeFirestore();
   }, [user]);
 
-  // const addSession = (newSession) => {
-  //   setSessions([...sessions, newSession]);
-  // };
+  const loadUserSessions = (currentUser) => {
+    // Logic to load user's sessions
+  };
 
   const addSession = async (newSession) => {
     if (user) {
@@ -54,8 +75,8 @@ function App() {
           <SessionList sessions={sessions} />
         </div>
       ) : (
-        <SignIn />
-      )}
+        <div>Sign In to view sessions</div>
+      )} 
     </div>
   );
 }
