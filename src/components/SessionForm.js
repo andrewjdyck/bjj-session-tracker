8929388b81108f991833a54fc75c70603a8315d6
andrewjdyck <andrew@andrewdyck.com>
2024-01-30 22:59:05 -0600

    Update SessionForm.js

--- src/components/SessionForm.js
+++ src/components/SessionForm.js
@@ -1,43 +1,107 @@
 import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
-const SessionForm = ({ onAddSession }) => {
+function SessionForm ({ onAddSession }) {
+  const [sessionData, setSessionData] = useState({
+    date: '',
+    startTime: '',
+    sessionType: '',
+    duration: 0, // Duration in minutes
+  });
+
+  const handleChange = (e) => {
+    setSessionData({ ...sessionData, [e.target.name]: e.target.value });
+  };
+
   const today = new Date();
-  const numberOfDaysToAdd = -2;
+  const numberOfDaysToAdd = -1;
   const dt = today.setDate(today.getDate() + numberOfDaysToAdd); 
   const defaultDateValue = new Date(dt).toISOString().split('T')[0]; // yyyy-mm-dd
   console.log(defaultDateValue);
   const [date, setDate] = useState(defaultDateValue);
   const [time, setTime] = useState('19:00');
   const [sessionType, setSessionType] = useState('no-gi');
+  const [duration, setDuration] = useState(90);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Assuming you'll add more validation here
-    onAddSession({ date, time, sessionType });
-    setDate('');
-    setTime('');
-    setSessionType('');
+    // onAddSession({ date, time, sessionType });
+    onAddSession({ sessionData });
+    setDate(defaultDateValue); // Reset the date
+    setTime('19:00'); // Reset the time
+    setSessionType(''); // reset the session type
+    setDuration(90); // reset the duration
   };
-
-  
 
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Date:</label>
-      <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label>Date</Form.Label>
+        <Form.Control
+          type="date"
+          name="date"
+          value={sessionData.date}
+          onChange={handleChange}
+        />
+      </Form.Group>
 
-      <label>Time:</label>
-      <input type="time" value={time} onChange={(e) => setTime(e.target.value)} required />
+      <Form.Group>
+        <Form.Label>Start Time</Form.Label>
+        <Form.Control
+          type="time"
+          name="startTime"
+          value={sessionData.startTime}
+          onChange={handleChange}
+        />
+      </Form.Group>
 
-      <label>Session Type:</label>
-      <select value={sessionType} onChange={(e) => setSessionType(e.target.value)} required>
-        <option value="gi">Gi Training</option>
-        <option value="no-gi">No-Gi Training</option>
-      </select>
+      <Form.Group>
+        <Form.Label>Session Type</Form.Label>
+        <Form.Control
+          as="select"
+          name="sessionType"
+          value={sessionData.sessionType}
+          onChange={handleChange}
+        >
+          <option value="">Select...</option>
+          <option value="gi">Gi</option>
+          <option value="no-gi">No-Gi</option>
+          {/* Add other session types as needed */}
+        </Form.Control>
+      </Form.Group>
 
-      <button type="submit">Log Session</button>
-    </form>
+      <Form.Group>
+        <Form.Label>Duration (minutes)</Form.Label>
+        <Form.Control
+          type="number"
+          name="duration"
+          value={sessionData.duration}
+          onChange={handleChange}
+          min="0"
+        />
+      </Form.Group>
+
+      <Button type="submit">Add Session</Button>
+    </Form>
+
+
+    // <form onSubmit={handleSubmit}>
+    //   <label>Date:</label>
+    //   <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+
+    //   <label>Time:</label>
+    //   <input type="time" value={time} onChange={(e) => setTime(e.target.value)} required />
+
+    //   <label>Session Type:</label>
+    //   <select value={sessionType} onChange={(e) => setSessionType(e.target.value)} required>
+    //     <option value="gi">Gi Training</option>
+    //     <option value="no-gi">No-Gi Training</option>
+    //   </select>
+
+    //   <button type="submit">Log Session</button>
+    // </form>
   );
 };
 
